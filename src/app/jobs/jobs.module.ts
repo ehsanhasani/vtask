0d1fcb74da1f8d05733ba5e00d1eb3b09301d656
ehsanhasani <ehsanhasani693@gmail.com>
2019-02-09 23:16:33 +0330

    create list jobs component under jobs module

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { JobsComponent } from './jobs.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [JobsComponent],
+  declarations: [JobsComponent, ListComponent],
   imports: [
     CommonModule
   ],
