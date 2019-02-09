4f9de8bcf33248c44c0c86b5ddffa36a6fbf3712
ehsanhasani <ehsanhasani693@gmail.com>
2019-02-09 23:18:29 +0330

    create list item component under jobs module for show job row

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { JobsComponent } from './jobs.component';
 import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
-  declarations: [JobsComponent, ListComponent],
+  declarations: [JobsComponent, ListComponent, ListItemComponent],
   imports: [
     CommonModule
   ],
