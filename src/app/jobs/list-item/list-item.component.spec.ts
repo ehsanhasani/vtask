54ef44892d7dadf36c05fb203ef257309f117b43
ehsanhasani <ehsanhasani693@gmail.com>
2019-02-10 10:19:25 +0330

    write test for list item component

--- src/app/jobs/list-item/list-item.component.spec.ts
+++ src/app/jobs/list-item/list-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListItemComponent } from './list-item.component';
+import { Job } from '../job';
 
 describe('ListItemComponent', () => {
   let component: ListItemComponent;
@@ -21,5 +22,32 @@ describe('ListItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should does not show job detail if job does not exist', () => {
+    const job: Job = null;
+
+    component.job = job;
+    fixture.detectChanges();
+    const jobElement: HTMLElement = fixture.nativeElement;
+    const h3 = jobElement.querySelector('h3');
+    expect(h3).toBe(null);
+  });
+
+  it('should show job detail title', () => {
+    const job: Job = {
+      position_title: 'Registered Nurse INTENSIVE CARE UNIT',
+      organization_name: 'Veterans Affairs, Veterans Health Administration',
+      start_date: '',
+      end_date: '',
+      locations: [],
+      url: ''
+    };
+
+    component.job = job;
+    fixture.detectChanges();
+    const jobElement: HTMLElement = fixture.nativeElement;
+    const h3 = jobElement.querySelector('h3');
+    expect(h3.textContent).toEqual(job.position_title);
   });
 });
