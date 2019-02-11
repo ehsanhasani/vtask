a4a96c5f58d750a266f3dfaf1306250a56323d7a
ehsanhasani <ehsanhasani693@gmail.com>
2019-02-11 11:44:10 +0330

    change route of jobs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { JobsComponent } from './jobs/jobs.component';
 import { MayanTombComponent } from './mayan-tomb/mayan-tomb.component';
 
 const routes: Routes = [
-  {path: '', component: JobsComponent},
+  {path: '', redirectTo: '/jobs', pathMatch: 'full'},
+  {path: 'jobs', component: JobsComponent},
   {path: 'mayan', component: MayanTombComponent},
 ];
 
