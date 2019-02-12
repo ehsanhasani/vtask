92cd649cb2ac5c98a712b0bcbd5b3a804a58e851
ehsanhasani <ehsanhasani693@gmail.com>
2019-02-12 13:10:41 +0330

    loading test

--- src/app/shared/loading/loading.service.spec.ts
+++ src/app/shared/loading/loading.service.spec.ts
@@ -9,4 +9,21 @@ describe('LoadingService', () => {
     const service: LoadingService = TestBed.get(LoadingService);
     expect(service).toBeTruthy();
   });
+
+  it('displayLoader be work correctly', () => {
+    const service: LoadingService = TestBed.get(LoadingService);
+    service.display = false;
+    service.displayLoader();
+    expect(service.display).toEqual(true);
+  });
+
+  it('displayloading be work correctly', () => {
+    const service: LoadingService = TestBed.get(LoadingService);
+    service.display = false;
+    service.displayloading.subscribe((data) => {
+      service.display = !data;
+    });
+    service.displayLoader();
+    expect(service.display).toEqual(false);
+  });
 });
